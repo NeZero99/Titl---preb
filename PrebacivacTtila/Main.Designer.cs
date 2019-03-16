3b4e9f04fb4c9493c2947e3c7351d7590047993e
NeZero99 <necer.radoicic@gmail.com>
2019-03-16 03:25:53 +0100

    Ubacivanje gotovog projekta

---
+++ PrebacivacTtila/Main.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace PrebacivacTtila
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnSacuvaj = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.lblIzmenjen = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Otvori Titl";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSacuvaj
+            // 
+            this.btnSacuvaj.Location = new System.Drawing.Point(446, 263);
+            this.btnSacuvaj.Name = "btnSacuvaj";
+            this.btnSacuvaj.Size = new System.Drawing.Size(75, 23);
+            this.btnSacuvaj.TabIndex = 1;
+            this.btnSacuvaj.Text = "Sacuvaj";
+            this.btnSacuvaj.UseVisualStyleBackColor = true;
+            this.btnSacuvaj.Click += new System.EventHandler(this.btnSacuvaj_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 40);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(509, 217);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // lblIzmenjen
+            // 
+            this.lblIzmenjen.AutoSize = true;
+            this.lblIzmenjen.Location = new System.Drawing.Point(12, 273);
+            this.lblIzmenjen.Name = "lblIzmenjen";
+            this.lblIzmenjen.Size = new System.Drawing.Size(122, 13);
+            this.lblIzmenjen.TabIndex = 4;
+            this.lblIzmenjen.Text = "Titl je uspesno izmenjen!";
+            this.lblIzmenjen.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(371, 15);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(150, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "by: Nemanja Radoičić - 2019 -";
+            // 
+            // Main
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 298);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblIzmenjen);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnSacuvaj);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.Text = "Prebacivac Titla";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Main_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Main_DragEnter);
+            this.DragLeave += new System.EventHandler(this.Main_DragLeave);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSacuvaj;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label lblIzmenjen;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ PrebacivacTtila/Main.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PrebacivacTtila
+{
+    public partial class Main : Form
+    {
+        private string originalTitl;
+        private string izmenjenTitl;
+        private string imeTitla;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    imeTitla = openFileDialog1.SafeFileName;
+                    izFajlaUString(openFileDialog1.FileName);
+
+                    lblIzmenjen.Visible = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void izFajlaUString(string putanja)
+        {
+            StreamReader rider = new StreamReader(putanja, Encoding.GetEncoding("Windows-1250"), true);
+            originalTitl = rider.ReadToEnd();
+            izmenjenTitl = prebacivanjeSlova (originalTitl);
+            richTextBox1.Text = izmenjenTitl;
+            rider.Dispose();
+            rider.Close();
+        }
+
+        private string prebacivanjeSlova(string titlZaPreb)
+        {
+            StringBuilder bilder = new StringBuilder(titlZaPreb);
+            //šđžčć ŠĐŽČĆ
+            for(int i = 0; i < bilder.Length; i++)
+            {
+                switch (bilder[i])
+                {
+                    case 'š':
+                        bilder[i] = 's';
+                        break;
+                    case 'đ':
+                        bilder[i] = 'd';
+                        break;
+                    case 'ž':
+                        bilder[i] = 'z';
+                        break;
+                    case 'č':
+                        bilder[i] = 'c';
+                        break;
+                    case 'ć':
+                        bilder[i] = 'c';
+                        break;
+                    case 'Š':
+                        bilder[i] = 'S';
+                        break;
+                    case 'Đ':
+                        bilder[i] = 'D';
+                        break;
+                    case 'Ž':
+                        bilder[i] = 'Z';
+                        break;
+                    case 'Č':
+                        bilder[i] = 'C';
+                        break;
+                    case 'Ć':
+                        bilder[i] = 'C';
+                        break;
+                }
+            }
+            return bilder.ToString();
+        }
+
+        private void btnSacuvaj_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog cuvanjeDialog = new SaveFileDialog();
+            cuvanjeDialog.FileName = imeTitla;
+            cuvanjeDialog.Filter = "Subtitles|*.srt";
+            cuvanjeDialog.Title = "Cuvanje izmenjenog titla";
+            cuvanjeDialog.RestoreDirectory = true;
+
+            if (cuvanjeDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter cuvanje = new StreamWriter(cuvanjeDialog.OpenFile(), Encoding.GetEncoding("Windows-1250"));
+                cuvanje.Write(izmenjenTitl);
+                cuvanje.Dispose();
+                cuvanje.Close();
+            }
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Main_DragEnter(object sender, DragEventArgs e)
+        {
+            vidljivostStavki(false);
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void Main_DragLeave(object sender, EventArgs e)
+        {
+            vidljivostStavki(true);
+        }
+
+        private void Main_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            izFajlaUString(files[0]);
+            imeTitla = izvlacenjeImenaIzPutanje(files[0]);
+            izmenjenTitl = prebacivanjeSlova(originalTitl);;
+            richTextBox1.Text = izmenjenTitl;
+
+            vidljivostStavki(true);
+
+            lblIzmenjen.Visible = true;
+        }
+
+        private string izvlacenjeImenaIzPutanje(string putanja)
+        {
+            string[] temp = putanja.Split('\\');
+
+            return temp[(temp.Length - 1)];
+        }
+
+        private void vidljivostStavki(bool vidljivost)
+        {
+            button1.Visible = vidljivost;
+            richTextBox1.Visible = vidljivost;
+            btnSacuvaj.Visible = vidljivost;
+            if (!vidljivost)
+            {
+                lblIzmenjen.Visible = vidljivost;
+            }
+        }
+    }
+}
